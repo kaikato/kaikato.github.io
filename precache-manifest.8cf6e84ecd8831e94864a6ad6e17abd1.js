a5cf1b069cb42a1408e35f68c547839cc92bd67f
Kai <kaiykato@stanford.edu>
2023-07-16 18:33:52 +0800

    Updates

---
+++ precache-manifest.8cf6e84ecd8831e94864a6ad6e17abd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d5dcf813d99efa54fd",
+    "url": "/react-gh-pages/static/css/main.45ff0bb7.chunk.css"
+  },
+  {
+    "revision": "74d5dcf813d99efa54fd",
+    "url": "/react-gh-pages/static/js/main.74d5dcf8.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-gh-pages/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "1938d0b7e11ebc2f0bd3db854693f54b",
+    "url": "/react-gh-pages/index.html"
+  }
+];
