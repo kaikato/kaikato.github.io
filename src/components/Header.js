cfb4dd87434d35681aba74622ce4c1e09019c7e6
Kai <kaiykato@stanford.edu>
2023-07-17 00:06:26 +0800

    new pp

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,6 +20,7 @@ export default class Header extends Component {
           </nav>
 
           <div className="row banner">
+
             <div className="banner-text">
               <h1 className="responsive-headline">Hi, I'm {resumeData.name}.</h1>
               <h3 style={{ color: '#fff', fontFamily: 'sans-serif ' }}>I am a {resumeData.role}. {resumeData.roleDescription}
