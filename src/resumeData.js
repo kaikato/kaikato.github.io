a5e873d9dabdeb334f7e97f320087d246ab57155
Kai <kaiykato@stanford.edu>
2023-07-16 18:17:37 +0800

    new repo

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+  "imagebaseurl": "https://github.com/kaikato",
+  "name": "Kai Kato",
+  "role": "Student at Stanford University",
+  "linkedinId": "kai-kato",
+  "roleDescription": "I am interested in cyber...",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/kai-kato",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/kaikato",
+      "className": "fa fa-github"
+    }
+  ],
+  "aboutme": "I am ...",
+  "address": "Stanford, CA",
+  "website": "https://kaikato.github.io",
+  "education": [
+    {
+      "UniversityName": "Stanford University",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2020",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "UniversityName": "Some University",
+      "specialization": "Some specialization",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "Some Company",
+      "specialization": "Some specialization",
+      "MonthOfLeaving": "Jan",
+      "YearOfLeaving": "2018",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
